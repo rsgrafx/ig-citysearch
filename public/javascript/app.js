0753e6b139046df1b9396172c91a93092cf189a2
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-02-18 13:57:43 -0600

    Added javascript to be served

---
+++ public/javascript/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var CityGram = angular.module('CityGram', []);
+
+CityGram.controller('SearchCtrl', function SearchCtrl() {
+  var site_home = this;
+  site_home.site_title = "Welcome to IG City Search";
+})
+
