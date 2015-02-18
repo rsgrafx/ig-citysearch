4dbdcc10c1e4bdf30c957892b8c2d33f6e044de8
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-02-18 14:22:28 -0600

    Basic form added to index view

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -2,8 +2,28 @@
 
 var CityGram = angular.module('CityGram', []);
 
-CityGram.controller('SearchCtrl', function SearchCtrl() {
+CityGram.controller('SearchCtrl', ['Pictures', function SearchCtrl(Pictures) {
   var site_home = this;
   site_home.site_title = "Welcome to IG City Search";
-})
 
+  site_home.fetch_data = function(content) {
+    console.log(content);
+  }
+}])
+
+CityGram.factory('Pictures', ['$http', '$q', function($http, $q) {
+  
+  var pics =  {
+    fetch: function(st_address, city, country) {
+      $http.post('http://localhost:9000/recent_pictures', { street_address: st_address, city: city, country: country })
+      .success(function(response, status, headers, config) {
+        pics.pictures = response.body
+      })
+      .error(function(response, status, headers, config) {
+        console.log(response.status);
+      })
+    }
+  }
+  return pics;
+
+}])
