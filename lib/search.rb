fc362a6d0069c7b230400db8f6b24d0ee9b8482f
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-02-22 10:32:08 -0600

    Updates and cleanup

--- lib/search.rb
+++ lib/search.rb
@@ -2,23 +2,19 @@ module Search
   def search(options)
 
     if options.fetch('coordinates', false)
-      # require 'pry'
-      # binding.pry
-
       lat = options['coordinates']['latitude']
       long = options['coordinates']['longitude']
-
       return Instagram.client.media_search(lat, long)
     end
-    
-    if options.is_a? Hash
-      _full_address = options.fetch('street_address', '')
-      _full_address << options.fetch('city', '')
-      _full_address << options.fetch('country', '')
-      if options.empty?
-        _full_address = 'Brooklyn, New York City'
-      end
+
+    _full_address = options.fetch('street_address', '')
+    _full_address << options.fetch('city', '')
+    _full_address << options.fetch('country', '')
+    # Last Resort * Hello Brooklyn
+    if options.empty?
+      _full_address = 'Brooklyn, New York City'
     end
+
     lat, long = get_coordinates(_full_address)
     Instagram.client.media_search(lat, long)
   end
