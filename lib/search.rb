7ced1ab36f4490fb450767475f0e97969891e0cb
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-02-25 14:16:23 -0600

    Wrote a few specs and refactored the logic in search

--- lib/search.rb
+++ lib/search.rb
@@ -2,35 +2,33 @@ require_relative 'search_item'
 
 module Search
   def search(options)
-    if options.fetch('coordinates', false)
-      lat = options['coordinates']['latitude']
-      long = options['coordinates']['longitude']
-      return Instagram.client.media_search(lat, long)
-    end
-
-    _full_address = options.fetch('street_address', '')
-    _full_address << options.fetch('city', '')
-    _full_address << options.fetch('country', '')
-    # Last Resort * Hello Brooklyn
-    if options.empty?
-      _full_address = 'Brooklyn, New York City'
-    end
-
-    lat, long = get_coordinates(_full_address)
-    Instagram.client.media_search(lat, long)
+    @options = options
+    return by_coordinates if @options.fetch('coordinates', false)
+    return by_address
   end
 
   def get_coordinates(address)
-    lat, long= Geocoder.coordinates address
-    [lat, long]
+    @lat, @long= Geocoder.coordinates address
+    [@lat, @long]
+  end
+
+  def by_coordinates
+    Instagram.client.media_search(@options['coordinates']['latitude'], @options['coordinates']['longitude'])
+  end
+
+  def by_address
+    _full_address =  @options.fetch('street_address', '')
+    _full_address <<','<< @options.fetch('city', '')
+    _full_address <<','<< @options.fetch('country', '')
+
+    if get_coordinates(_full_address).compact.empty?
+      get_coordinates('San Pedro Town, Belize, Central America')
+    else
+      Instagram.client.media_search(@lat, @long)
+    end
   end
 
   def save(options)
-    # if options['longitude'].to_s.empty?
-    #   lat, long= Geocoder.coordinates(options['city_name'])
-    #   options['longitude'] = long
-    #   options['latitude'] = lat
-    # end
     searchItem = SearchItem.new(options)
     _result = searchItem.save
     searchItem
--- lib/setup.rb
+++ lib/setup.rb
@@ -1,7 +1,7 @@
 module Setup
 
   def initialize_instagram
-    setup if ENV['RACK_ENV'] == 'development'
+    setup if ENV['RACK_ENV'] == 'development' || ENV['RACK_ENV'] == nil
     Instagram.configure do |config|
       config.client_id = ENV.fetch('IG_CLIENT_ID')
     end
