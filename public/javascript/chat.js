14c74a95d985b5dfc5a525beec11b6193fefc5f3
Orion Engleton - MBAir <orion.engleton@gmail.com>
2015-02-26 10:26:36 -0600

    Modified chat.js file / created disabled file.

--- public/javascript/chat.js
+++ public/javascript/chat.js
@@ -4,10 +4,16 @@ window.onload = function(){
       return function(msg){ el.innerHTML = msg + '<br />' + el.innerHTML; }
     }(document.getElementById('msgs'));
 
+    var disable_form = function(el) {
+      $('#chat-form input').prop('disabled', true);
+    }
+
     var ws       = new WebSocket('ws://' + window.location.host + window.location.pathname);
     ws.onopen    = function()  { console.log('websocket opened'); };
-    ws.onclose   = function()  { console.log('Shout Outs closed'); }
-    ws.onmessage = function(m) { show('YOFrom#: ' +  m.data); };
+    ws.onclose   = function()  { //console.log('Shout Outs closed'); 
+      disable_form()
+    }
+    ws.onmessage = function(m) { show('YO From#: ' +  m.data); };
 
     var sender = function(f){
       var input     = document.getElementById('input');
